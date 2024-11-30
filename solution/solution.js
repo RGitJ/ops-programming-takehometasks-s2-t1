243c97f7b89609be31741f8384420370a28cb50b
RGitJ <rohanjagannathan@outlook.com>
2024-11-29 21:00:18 -0500

    Began CLI user interface and initialized sort type option functionality

--- solution/solution.js
+++ solution/solution.js
@@ -1,4 +1,12 @@
 
+const sortTypes = ["mr", "sd", "ra", "pa"]
+let apiUrl = "https://devpost.com/api/hackathons"
+
+// Get proper api url based on sort type
+const getApiUrl = (sortType) => {
+    const baseUrl = "https://devpost.com/api/hackathons";
+}
+
 // Get hackathons
 const getHackathons = async () => {
     const apiUrl = "https://devpost.com/api/hackathons";
@@ -17,4 +25,9 @@ const getHackathons = async () => {
     }
 }
 
-console.log(getHackathons())
+// Command Line User Interface
+console.log("Hello! Welcome to the DevPost Hackathons API command line interface!")
+const sortTypePrompt = prompt("How would you like to sort your hackathons? (Most Relevant - mr, Submission date - sd, Recently added - ra, Prize amount - pa): ")
+if (!sortTypes.includes(sortTypePrompt)) {
+    console.log("Please try again! Type the characters after the dash after the way in which wyou would like to sort your hackathons!")
+}
